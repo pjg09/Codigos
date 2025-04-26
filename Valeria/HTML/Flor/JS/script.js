b49dcfba8578d1d7bfcfe678cc9772b3e62facc0
Pedro <pedroj1229@gmail.com>
2025-04-26 16:15:27 -0500

    Eliminacion

--- Valeria/HTML/Flor/JS/script.js
+++
@@ -1,9 +0,0 @@
-alert("Encuentra el lugar para dar click y ser redireccionada")
-
-function redireccion() {
-    window.location.href = 'Paginas/redireccion.html';
-}
-
-function alerta() {
-    alert('Lugar incorrecto');
-}
